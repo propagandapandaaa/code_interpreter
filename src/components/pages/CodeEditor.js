8af1915463aacdf79bdeb34a1cc9864092492c06
jacobs <jacobs@mathesio.cz>
2024-05-11 01:23:06 +0200

    feat: minor visual changes

--- src/components/pages/CodeEditor.js
+++ src/components/pages/CodeEditor.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Editor from '@monaco-editor/react';
+import './CodeEditor.css';
 
 function CodeEditor() {
     return (
         <div className="code-editor-container">
-            <Editor theme="vs-dark" height="500px" defaultLanguage='python' defaultValue='// Edit your code here' />
+            <div className='code-editor'>
+                <Editor theme="vs-dark" height="500px" defaultLanguage='javascript' defaultValue='// Edit your code here' />
+            </div>
         </div>
     );
 }
