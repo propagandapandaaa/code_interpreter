66791a4ec8be4585f5cd67a095f97210afddbc46
jacobs <daniel.jacobs@outlook.cz>
2024-05-11 13:09:44 +0200

    refactor: removed unused code

--- src/components/pages/CodeEditor.js
+++ src/components/pages/CodeEditor.js
@@ -13,17 +13,3 @@ function CodeEditor() {
 }
 
 export default CodeEditor;
-
-
-// import './CodeEditor.css';
-// import Editor from '@monaco-editor/react';
-
-// const CodeEditor = () => {
-//     return (
-//         <div className="code-editor-container" style={{ paddingTop: '0px', borderRadius: '30px' }}>
-//             <Editor theme="vs-dark" height="500px" defaultLanguage='python' defaultValue='// Edit your code here' />
-//         </div>
-//     );
-// };
-
-// export default CodeEditor;
